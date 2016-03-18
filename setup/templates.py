b6155caa2f18b9f4e72a4f28a8943ef78714f2f9
Joe DeRose <joe.derose13@berkeley.edu>
2016-03-18 12:13:10 -0700

    Updated config scripts

--- setup/templates.py
+++ setup/templates.py
@@ -1,7 +1,9 @@
 from __future__ import print_function
 from abc import ABCMeta, abstractmethod
 from glob import glob
+import shutil
 import yaml
+import stat
 import os
 
 
@@ -281,7 +283,27 @@ class PixLC(Template):
 
 class CalcRnn(Template):
 
+    def readConfigTemplateFile(self):
+
+        templatefile = os.path.join('config', '%s.cfg' % self.__class__.__name__)
+        
+        with open(templatefile, 'r') as fp:
+            halocfgtemp = fp.readlines()
+
+        cfgtemp = []
+        
+        for l in halocfgtemp:
+            if 'Halo' not in l:
+                cfgtemp.append(l)
+        
+        self.cfgtemp = "".join(cfgtemp)
+        self.halocfgtemp = "".join(halocfgtemp)
+
     def write_config(self, opath, boxl):
+
+        osp = opath.split('/')
+        osp[-1] = 'halos/out_0.parents'
+        halopath = '/'.join(osp)
 
         pars = {}
         pars['SimType'] = self.cosmoparams['SimType']
@@ -297,10 +319,16 @@ class CalcRnn(Template):
         pars['NRnn'] = self.cosmoparams['NRnn'][boxl]
         pars['OPath'] = opath
         pars['BBoxFile'] = '{0}/bboxindex.txt'.format(opath)
+        pars['HFile'] = halopath
         cfg = self.cfgtemp.format(**pars)
+        hcfg = self.halocfgtemp.format(**pars)
 
-        with open('{0}/calcrnn.cfg'.format(jobbase), 'w') as fp:
+        with open('{0}/calcrnn_parts.cfg'.format(jobbase), 'w') as fp:
             fp.write(cfg)
+
+        with open('{0}/calcrnn_halos.cfg'.format(jobbase), 'w') as fp:
+            fp.write(hcfg)
+
 
 
     def write_jobscript(self, opath, boxl):
@@ -339,30 +367,44 @@ class Addgals(Template):
     def write_config(self, opath, boxl):
         pars = {}
         bopath = '/'.join(opath.split('/')[:-1])
-        bsbase = bopath.split('Lb')
-        pars['SimName'] = self.cosmoparams['SimName']
+        bsbase = bopath.split('Lb{0}'.format(boxl))
+        sn = '{0}-{1}'.format(self.cosmoparams['SimName'], self.simnum)
+        pars['SimName'] = "'{0}'".format(sn)
         pars['SimNum'] = self.simnum
-        pars['Halos'] = "'{0}'+boxsize'+'{1}/{2}'".format(bsbase[0]+'Lb',bsbase[1], 'halos/out_0.parents')
-        pars['HaloRnn'] = "'{0}'+boxsize'+'{1}/{2}'".format(bsbase[0]+'Lb',bsbase[1], 'rnn/rnn_out_0.parents')
-        pars['LCDir'] = "'{0}'+boxsize'+'{1}/{2}/'".format(bsbase[0]+'Lb',bsbase[1], 'pixlc')
-        pars['SDir'] = os.path.join(self.sysparams['ExecDir'],self.__class__.__name__)
+        pars['Boxl' ] = "'{0}'".format(boxl)
+        pars['Halos'] = "'{0}/{1}'".format(bopath, 'halos/out_0.parents')
+        pars['HaloRnn'] = "'{0}/{1}'".format(bopath, 'rnn/rnn_out_0.parents')
+        pars['LCDir'] = "'{0}/{1}/'".format(bopath, 'pixlc')
+        pars['SDir'] = "'{0}'".format(os.path.join(self.sysparams['ExecDir'],self.__class__.__name__))
+        pars['OmegaM'] = self.cosmoparams['OmegaM']
+        pars['OmegaL'] = self.cosmoparams['OmegaL']
+        pars['ZMin'] = self.cosmoparams['SimZmin'][boxl]
+        pars['ZMax'] = self.cosmoparams['SimZmax'][boxl]
+        pars['NZbins'] = self.cosmoparams['NZbins'][boxl]
+        pars['BCGMassLim'] = "'{0}'".format(self.cosmoparams['BCGMassLim'][boxl])
+        
         jobbase = os.path.join(self.sysparams['JobBase'], 
-                               '{0}-{1}'.format(pars['SimName'], pars['SimNum']),
+                               '{0}'.format(sn),
                                'Lb{0}'.format(boxl), self.__class__.__name__)
-        pars['OPath'] = opath
+        pars['OPath'] = "'{0}'".format(opath)
+        pars['PFile'] = "'{0}'".format(self.cosmoparams['ParamFile'])
         cfg = self.cfgtemp.format(**pars)
 
+        shutil.copyfile("{0}/scripts/make_buzzard_flock.pro".format(pars['SDir'][1:-1]),
+                        "{0}/make_buzzard_flock.pro".format(jobbase))
+        shutil.copyfile("{0}/scripts/make_params_files_buzzard.sh".format(pars['SDir'][1:-1]),
+                        "{0}/make_params_files_buzzard.sh".format(jobbase))
+        shutil.copyfile("{0}/scripts/make_l-addgals_submission_files.sh".format(pars['SDir'][1:-1]),
+                        "{0}/make_l-addgals_submission_files.sh".format(jobbase))
+        os.chmod("{0}/make_params_files_buzzard.sh".format(jobbase), 0o777)
+        os.chmod("{0}/make_l-addgals_submission_files.sh".format(jobbase), 0o777)
         with open('{0}/setup_addgals.idl'.format(jobbase), 'w') as fp:
             fp.write(cfg)
 
 
     def write_jobscript(self, opath, boxl):
-        osp = opath.split('/')
-        osp[-1] = 'pixlc/'
-        lcpath = '/'.join(osp)
         pars = {}
         pars['BoxL'] = boxl
-
         pars['SimName'] = self.cosmoparams['SimName']
         pars['SimNum'] = self.simnum
         pars['Repo'] = self.sysparams['Repo']
@@ -371,21 +413,10 @@ class Addgals(Template):
         jobbase = os.path.join(self.sysparams['JobBase'], 
                                '{0}-{1}'.format(pars['SimName'], pars['SimNum']),
                                'Lb{0}'.format(boxl), self.__class__.__name__)
-        pars['NameFile'] = '{0}/{1}-{2}_Lb{3}.txt'.format(jobbase, pars['SimName'],
-                                                          pars['SimNum'], boxl)
-        pars['ExecDir'] = os.path.join(self.sysparams['ExecDir'],self.__class__.__name__)
-        pars['OPath'] = opath
         pars['Email'] = self.sysparams['Email']
         
         jobscript = self.jobtemp.format(**pars)
         
-        #write the lightcone files to be read by pixlc
-        lcfiles = glob('{0}/*'.format(lcpath))
-
-        with open(pars['NameFile'], 'w') as fp:
-            fbuff = '\n'.join(lcfiles)
-            fp.write(fbuff)
-
-        with open('{0}/job.rnn.{1}'.format(jobbase, self.sysparams['Sched']), 'w') as fp:
+        with open('{0}/job.adg.{1}'.format(jobbase, self.sysparams['Sched']), 'w') as fp:
             fp.write(jobscript)
     
