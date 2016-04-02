d6e8d184c692b8f5f872a4683485f9fa21fbfa11
Joe <joe.derose13@gmail.com>
2016-04-02 13:48:30 -0700

    Removed boxl param from all.sh template

--- systems/sherlock/all.sh
+++ systems/sherlock/all.sh
@@ -2,8 +2,8 @@
 #SBATCH -p iric
 #SBATCH --qos iric
 #SBATCH -t {TimeLimitHours}:00:00
-#SBATCH -J {SimName}{SimNum}-all-Lb{BoxL}
-#SBATCH -o {SimName}{SimNum}-all-Lb{BoxL}.%j.oe
+#SBATCH -J {SimName}{SimNum}-all
+#SBATCH -o {SimName}{SimNum}-all.%j.oe
 #SBATCH --mail-type=All
 #SBATCH --mail-user  {Email}
 #SBATCH -N {NNodes}
