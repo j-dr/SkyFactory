af581bee5aa3c6dfd1b29f413a57371dda9c5e8e
Joe DeRose <joe.derose13@berkeley.edu>
2016-04-12 09:55:33 -0700

    Wrote postprocessing submission scripts for sherlock

---
+++ systems/sherlock/addgalspostprocess.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p iric
+#SBATCH --qos iric
+#SBATCH -t {TimeLimitHours}:00:00
+#SBATCH -J {SimName}{SimNum}-adgpp
+#SBATCH -o {SimName}{SimNum}-adgpp.%j.oe
+#SBATCH --mail-type=All
+#SBATCH --mail-user  {Email}
+#SBATCH -N {NNodes}
+#SBATCH --exclusive
+
+srun -n {NCores} python {ExecDir}/scripts/finalize_catalog.py addgalspostprocess.cfg
