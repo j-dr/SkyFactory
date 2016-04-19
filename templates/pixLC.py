fc68bf4ce3782be4508e31bea4781697e4b6c23f
Joe <joe.derose13@gmail.com>
2016-04-18 20:14:01 -0700

    Fixed halopath in pixlc

--- templates/pixLC.py
+++ templates/pixLC.py
@@ -42,7 +42,7 @@ class PixLC(BaseTemplate):
         lcpath = '/'.join(osp)
         pars = {}
         pars['BoxL'] = boxl
-        pars['HaloDir'] = "{0}/Lb{1}/halos/".format(self.getOutputBaseDir(), boxl)
+        pars['HaloDir'] = "{0}/Lb{1}/output/halos/".format(self.getOutputBaseDir(), boxl)
         pars['SimName'] = self.cosmoparams['Simulation']['SimName']
         pars['SimNum'] = self.simnum
         pars['ExecDir'] = self.getExecDir()
