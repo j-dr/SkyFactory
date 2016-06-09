720c2c7aa63b13c8dc6dc7b9c696eeea896c2a1c
j-dr <joe.derose13@berkeley.edu>
2016-06-09 13:01:34 -0400

    Updated errormodel template, now includes rotation

--- templates/errormodel.py
+++ templates/errormodel.py
@@ -13,33 +13,63 @@ class ErrorModel(BaseTemplate):
         super(ErrorModel, self).__init__(simnum, system, cosmo, allboxes=True)
 
     def write_config(self, opath, boxl):
-
         pars = {}
         pars['GalPath']  = os.path.join(self.getOutputBaseDir(), 'addgalspostprocess', 'truth', '*lensed*')
+
         jbase = os.path.join(self.getJobBaseDir(), "errormodel")
+
         for i in range(self.cosmoparams['ErrorModel']['NModels']):
 
             pars['Model']    = self.cosmoparams['ErrorModel']['Models'][i]
-            pars['OutBase']  = os.path.join(self.getOutputBaseDir(), 'addgalspostprocess', pars['Model'])
-            pars['DepthFile']= self.cosmoparams['ErrorModel']['DepthFile'][i].format(**self.sysparams)
+            pars['RotOutDir'] = os.path.join(self.getOutputBaseDir(),
+                                            'addgalspostprocess',
+                                            'truth_rotated_{0}'.format(pars['Model']))
+            pars['RotBase']   = "{0}-{1}{2}_{3}_truth.fits".format(
+                self.cosmoparams['Simulation']['SimName'], self.simnum,
+                pars['Model'],
+                self.cosmoparams['Simulation']['ModelVersion'])
+            pars['MatPath'] = self.cosmoparams['ErrorModel']['MatPath'][i].format(**self.sysparams)
+            pars['OutputDir']  = os.path.join(self.getOutputBaseDir(), 'addgalspostprocess', pars['Model'])
+            pars['OutputBase'] = "{0}-{1}{2}_{3}".format(
+                self.cosmoparams['Simulation']['SimName'], self.simnum,
+                pars['Model'],
+                self.cosmoparams['Simulation']['ModelVersion'])
+            pars['DepthFile'] = self.cosmoparams['ErrorModel']['DepthFile'][i].format(**self.sysparams)
+            pars['Nest'] = self.cosmoparams['ErrorModel']['Nest']
 
-        if 'MagType' in self.cosmoparams['ErrorModel']:
-            pars['MagPath']  = os.path.join(self.getOutputBaseDir(), 'addgalspostprocess', 'mags',
+            if 'MagType' in self.cosmoparams['ErrorModel']:
+                pars['MagPath']  = os.path.join(self.getOutputBaseDir(),
+                                            'addgalspostprocess', 'mags',
                                             "*"+self.cosmoparams['ErrorModel']['MagType'][i]+"*")
-            pars['UseMags']  = True
-        else:
-            pars['MagPath']  = None
-            pars['UseMags']  = False
+                pars['UseMags']  = '[0,1,2,3,4]'
+            else:
+                pars['MagPath']  = None
+                pars['UseMags']  = None
 
-        if 'Rotation' in self.cosmoparams['ErrorModel']:
-            pars['Rotation']  = self.cosmoparams['ErrorModel']['Rotation'][i].format(**self.sysparams)
+            pars['DataBaseStyle'] = True
+            if 'Y1' in pars['Model']:
+                pars['Bands'] = '[g, r, i, z, y]'
+            elif 'DR8' in pars['Model']:
+                pars['Bands'] = '[u, g, r, i, z]'
+            else:
+                raise(ValueError("No bands specified for model {0}".format(pars["Model"])))
+
+            pars['UseLMAG'] = True
 
         with open("{0}/errormodel.{1}.cfg".format(jbase, i), 'w') as fp:
             fp.write("Model    : {Model}\n".format(**pars))
-            fp.write("OutBase  : {OutBase}\n".format(**pars))
+            fp.write("OutputDir  : {OutputDir}\n".format(**pars))
+            fp.write("OutputBase : {OutputBase}\n".format(**pars))
             fp.write("DepthFile: {DepthFile}\n".format(**pars))
+            fp.write("Nest: {Nest}\n".format(**pars))
+            fp.write("DataBaseStyle: {DataBaseStyle}".format(**pars))
+            fp.write("Bands: {Bands}".format(**pars))
+            fp.write("UseLMAG: {UseLMAG}".format(**pars))
             fp.write("MagPath  : {MagPath}\n".format(**pars))
             fp.write("UseMags  : {UseMags}\n".format(**pars))
+            fp.write("RotOutDir: {RotOutDir}\n".format(**pars))
+            fp.write("RotBase: {RotBase}\n".format(**pars))
+            fp.write("MatPath: {MatPath}\n".format(**pars))
             if 'Rotation' in pars.keys():
                 fp.write("Rotation : {Rotation}\n".format(**pars))
 
