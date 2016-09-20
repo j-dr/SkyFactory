ee3af8b6715ff5e779665541dca9337d602fc240
j-dr <joe.derose13@berkeley.edu>
2016-09-20 16:50:32 -0700

    Added edison submission scripts that weren't provided before

---
+++ systems/edison/surveymags.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p regular
+#SBATCH -A {Repo}
+#SBATCH -t {TimeLimitHours}:00:00
+#SBATCH -J {SimName}{SimNum}-sm
+#SBATCH -o {SimName}{SimNum}-sm.%j.oe
+#SBATCH --mail-type=All
+#SBATCH --mail-user  {Email}
+#SBATCH -N {NNodes}
+#SBATCH --exclusive
+
+cat magcommands.txt | srun -n {NTasks} -c {NCoresPerTask} minions
