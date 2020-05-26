c8e6c6e1b33429c85f802cba14ce54fed8bfbcfd
Joe DeRose <joe.derose13@berkeley.edu>
2020-05-26 13:09:10 -0700

    added repo setup script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+email=$1
+
+mkdir exec
+cd exec
+git clone https://github.com/j-dr/pixLC.git
+git clone https://github.com/j-dr/calcrnn.git
+git clone https://github.com/j-dr/pyaddgals addgals
+git clone https://github.com/j-dr/calclens
+
+module unload PrgEnv-gnu
+module load PrgEnv-intel cray-fftw cfitsio cray-hdf5 gsl
+
+cd calcrnn && make && cd -
+cd calclens && make && cd -
+cd ..
+
+sed -i "s,JobBase.*,JobBase: ${PWD}/chinchilla-herd/," systems/cori-haswell/cori-haswell.yaml
+sed -i "s,ExecDir.*,ExecDir: ${PWD}/exec/," systems/cori-haswell/cori-haswell.yaml
+sed -i "s,Email.*,Email: ${email}," systems/cori-haswell/cori-haswell.yaml 
